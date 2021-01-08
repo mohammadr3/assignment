e9832620e36fd79b8bd0edde140e90bc1a8d5220
mohammadr3 <72926701+mohammadr3@users.noreply.github.com>
2021-01-08 16:24:10 +0200

    Update s.js

--- js/s.js
+++ js/s.js
@@ -1,3 +1,14 @@
+var cont = 0 ;
+var a=[];
+function div_builder(arr_length){
+    for(var i = 0 ; i < arr_length ; i++ ){
+        document.querySelector(".lol").innerHTML=document.querySelector(".lol").innerHTML+"<div class=lol_child " +"  id = div_num" + i + "> </div>" ;
+        a[i]="#div_num"+i;
+    }
+    
+};
+
+
 document.addEventListener("DOMContentLoaded",function(event){
 
 
@@ -9,21 +20,24 @@ document.addEventListener("DOMContentLoaded",function(event){
             if ((this.readyState==3)){if(i==f){
                 document.querySelector(".lol").innerHTML="";
                 i++;
-            }}
+            }
+        }
 
 
         if ((this.readyState==4)&&(this.status==200)){
             
             var jsondata = JSON.parse(this.responseText);//from json to obj.
+            div_builder(jsondata.length) 
             for(var prop in jsondata){
-                document.querySelector(".lol").innerHTML=document.querySelector(".lol").innerHTML+ "<br>" + " name : " + jsondata[prop].name + "   <===>   description : "+jsondata[prop].description +"<br>";
+                
+                document.querySelector(a[prop]).innerHTML=document.querySelector(a[prop]).innerHTML+ "<br>" + " name : " + jsondata[prop].name + "   |<====================================>|   description : "+jsondata[prop].description +"<br>";
              }
              f++;
             
             }
         };
 
-        xttp.open("GET" , "js/chicken.json" , true ) ; //(POST in body , GET in url)
+        xttp.open("GET" , "/js/chicken.json" , true ) ; //(POST in body , GET in url)
         xttp.send(null) ; // for post only   
 
     })
@@ -33,22 +47,27 @@ document.addEventListener("DOMContentLoaded",function(event){
         var xttp = new XMLHttpRequest ();
         xttp.onreadystatechange=function(){
             var i = 0 , f = 0;
-           
             if ((this.readyState==3)){if(i==f){
                 document.querySelector(".lol").innerHTML="";
                 i++;
-            }}
+            }
+        }
+
+
         if ((this.readyState==4)&&(this.status==200)){
             
             var jsondata = JSON.parse(this.responseText);//from json to obj.
+            div_builder(jsondata.length) 
             for(var prop in jsondata){
-                document.querySelector(".lol").innerHTML=document.querySelector(".lol").innerHTML+ "<br>" + " name : " + jsondata[prop].name + "   <===>   description : "+jsondata[prop].description +"<br>";
+                
+                document.querySelector(a[prop]).innerHTML=document.querySelector(a[prop]).innerHTML+ "<br>" + " name : " + jsondata[prop].name + " |<====================================>| description : "+jsondata[prop].description +"<br>";
              }
-            f++;
+             f++;
+            
             }
         };
 
-        xttp.open("GET" , "js/two.json" , true ) ; //(POST in body , GET in url)
+        xttp.open("GET" , "/js/two.json" , true ) ; //(POST in body , GET in url)
         xttp.send(null) ; // for post only   
 
     })
@@ -57,24 +76,28 @@ document.addEventListener("DOMContentLoaded",function(event){
 
         var xttp = new XMLHttpRequest ();
         xttp.onreadystatechange=function(){
-
             var i = 0 , f = 0;
             if ((this.readyState==3)){if(i==f){
                 document.querySelector(".lol").innerHTML="";
                 i++;
-            }}
+            }
+        }
+
 
         if ((this.readyState==4)&&(this.status==200)){
             
             var jsondata = JSON.parse(this.responseText);//from json to obj.
+            div_builder(jsondata.length) 
             for(var prop in jsondata){
-                document.querySelector(".lol").innerHTML=document.querySelector(".lol").innerHTML+ "<br>" + " name : " + jsondata[prop].name + "   <===>   description : "+jsondata[prop].description +"<br>";
+                
+                document.querySelector(a[prop]).innerHTML=document.querySelector(a[prop]).innerHTML+ "<br>" + " name : " + jsondata[prop].name + "   |<====================================>|   description : "+jsondata[prop].description +"<br>";
              }
-            f++;
+             f++;
+            
             }
         };
 
-        xttp.open("GET" , "js/three.json" , true ) ; //(POST in body , GET in url)
+        xttp.open("GET" , "/js/three.json" , true ) ; //(POST in body , GET in url)
         xttp.send(null) ; // for post only   
 
     })
